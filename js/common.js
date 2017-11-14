086fa58df88926f066f3d8f29a49c182c4b0c8de
CAN-OD <824030278@163.com>
2017-11-14 10:37:02 +0800

    rem 移动端转换方法

--- js/common.js
+++ js/common.js
@@ -1062,7 +1062,19 @@
         }
         return _this.sizeDom;
     };
-
+    // 设置自适应 字体大小 用于rem 单位移动端
+    /**
+     * [remSize description]
+     * @param  {[type]} size [设计图大小]
+     * @return {[type]}      [description]
+     */
+    myFrame.remSize = function(size){
+        var whdef = 100 / size; // 表示1920的设计图,使用100PX的默认值
+        var wH = window.innerHeight; // 当前窗口的高度
+        var wW = window.innerWidth; // 当前窗口的宽度
+        var rem = wW * whdef; // 以默认比例值乘以当前窗口宽度,得到该宽度下的相应FONT-SIZE值
+        $('html').css('font-size', rem + "px");
+    };
 
     // };
 
@@ -1077,7 +1089,6 @@
     window.myFrame = myFrame;
     myFrame.init();
     // });
-
 
     $.fn.paging = function() {
 
